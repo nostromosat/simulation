74b4a835acf1960d96d771e3e6901cdee677fd78
CedricRom <46740745+CedricRom@users.noreply.github.com>
2019-03-05 09:38:38 +0100

    Update Optimization.java
    Updated, print every "better solution" anytime a new one is find

--- Nostromo_enum/src/main/Optimization.java
+++ Nostromo_enum/src/main/Optimization.java
@@ -6,6 +6,7 @@ import java.util.List;
 import cost.CostModel;
 import cargo.Launcher;
 import cargo.Mass;
+import cargo.Panel;
 import cargo.Propulsion;
 import asteroid.Asteroid;
 
@@ -13,13 +14,13 @@ public class Optimization {
 
 	private static String costformat(double cost){
 		if(cost>1e10)
-			return ""+Math.round(cost/1e7)/1e9+" G€";
+			return ""+Math.round(cost/1e7)/1e2+" G€";
 		else if(cost>1e7)
-			return ""+Math.round(cost/1e4)/1e6+" M€";
+			return ""+Math.round(cost/1e4)/1e2+" M€";
 		else if(cost>1e4)
-			return ""+Math.round(cost/10)/1e3+" k€";
+			return ""+Math.round(cost/10)/1e2+" k€";
 		else 
-			return ""+Math.round(cost/1e-2)+" €";
+			return ""+Math.round(cost/1e-2)/1e2+" €";
 	}
 
 
@@ -30,35 +31,46 @@ public class Optimization {
 		List<Mission> missions_done = new ArrayList<Mission>();
 		Asteroid target = Asteroid.ARM_341843_2008_EVS;
 		int n = 0;
-		int nMax = 5;
+		int nMax = 6;
 		int best_ind = 0;
 		double min_price = Double.MAX_VALUE;
 
-		// Il faut fixer la masse des panneaux plus efficacement, et surtout la masse du fuel !! ici 1000 panel, 2000 fuel.
-		int panel_mass = 1000;
-		int max_fuel_mass = 3000;
-		int max_ore_mass = 10000;
+		// Il faut fixer la masse des panneaux plus efficacement
+		int fuel_mass = 5000;
+		int max_ore_mass = 50000;
+		Propulsion propu = Propulsion.PPS1350_safran;
 
-		for(int ore_mass=500;ore_mass<max_ore_mass;ore_mass += 500){
-		//	for(int fuel_mass=500; fuel_mass < max_fuel_mass; fuel_mass += 500){
-				for(Propulsion propu : Propulsion.values()){
-					for(int i=1;i<nMax;i++){
+		for(int ore_mass=250;ore_mass<=max_ore_mass;ore_mass += 250){
+				//for(Propulsion propu : Propulsion.values()){
+					for(int i=1;i<=nMax;i++){
 						for(Launcher launch : Launcher.values()){
 							n++;
 							propu.setnEngine(i);
-							Mass miss_mass = new Mass(panel_mass,propu.getDryMass()*propu.getnEngine(),max_fuel_mass,ore_mass);
-							Mission miss = new Mission(target,miss_mass,propu,launch);
-							double kgcost = miss.getKgCost();
+							Panel pan = new Panel(propu,target);
+							Mass miss_mass = new Mass(pan.getMass(),
+									propu.getDryMass()*propu.getnEngine(),
+									fuel_mass,
+									ore_mass
+									);
+							Mission miss = new Mission(target,miss_mass,propu,launch,pan);
+							double kgcost =miss.getKgCost();
+							//System.out.println("kgCost: "+kgcost);
 							if(kgcost < min_price){
+								System.out.println("Ore: "+ore_mass+" -- "+
+													propu.getnEngine()+" "+
+													propu.getName()+" "+
+													launch.getLauncher()+" "+
+													launch.getOrbit()+" "+
+													" = "+kgcost+" €/kg"
+													);
 								missions_done.add(miss);
 								min_price = kgcost;
-								best_ind = missions_done.size();
-							}
+								best_ind = missions_done.size()-1;
+					//		}
 						}
 					}
-		//		}
-			}
-		}
+				}
+			} 
 		System.out.println("Number of cases tried: "+n+".");
 		System.out.println("Minimum price per kg achieved: "+costformat(min_price)+" for mission n°"+best_ind+".");
 
@@ -66,5 +78,19 @@ public class Optimization {
 		long endTime = System.currentTimeMillis();
 
 		System.out.println("\nThat took " + Math.round((endTime - startTime)/10)/100 + " seconds");
+		
+		Mission best = missions_done.get(best_ind);
+		System.out.println("Best case caracteristics:");
+		best.launcher.showDetails();
+		best.nostromo.showDetails();
+		best.nostromo.getMass().showDetails();
+		System.out.println("");
+		best.nostromo.getPropulsion().showDetails();
+		System.out.println("Mission using "+best.nostromo.getPropulsion().getnEngine()+" engines.\n");
+		System.out.println(new CostModel(best.duration,
+				best.nostromo.getMass(),
+				best.nostromo.getPropulsion().getPower()).bothCosts(best.nostromo.getMass().getOreMass()));
+		System.out.println("\ndV total: "+best.dV_total);
+		System.out.println("Total duration: "+best.duration);		
 	}
 }
